c00a942d5f70d3385716a7634ac86e16c075655f
code-walker-23 <chhabravinay549@gmail.com>
2025-05-23 22:01:25 +0530

    Initial commit: Set up Spring Boot project for Student Course Enrollment System

---
+++ student-course-enrollment/src/main/java/com/vinay/studentenrollment/StudentCourseEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.studentenrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseEnrollmentApplication.class, args);
+	}
+
+}
---
+++ student-course-enrollment/src/test/java/com/vinay/studentenrollment/StudentCourseEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.studentenrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
