53ba287923207eaed7f344b337ba027581ec47a7
code-walker-23 <chhabravinay549@gmail.com>
2025-05-24 23:33:25 +0530

    Add JPA repositories for User

---
+++ student-course-enrollment/src/main/java/com/vinay/studentenrollment/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.vinay.studentenrollment.repository;
+
+import com.vinay.studentenrollment.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
