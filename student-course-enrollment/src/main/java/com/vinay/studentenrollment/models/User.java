4879b6a09ef584cc8440941da1b1e22ba88153aa
code-walker-23 <chhabravinay549@gmail.com>
2025-05-24 23:27:05 +0530

    Add model classe: User

---
+++ student-course-enrollment/src/main/java/com/vinay/studentenrollment/models/User.java
@@ -0,0 +1,19 @@
+package com.vinay.studentenrollment.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role; // e.g., ROLE_ADMIN or ROLE_STUDENT
+
+}
+
